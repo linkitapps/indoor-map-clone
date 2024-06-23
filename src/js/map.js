1bb9bb487249b0ea9ae6ee326bc1caa3769a7690
linkitapps <master@linkit.kr>
2024-06-24 06:47:35 +0900

    maplibre 데모에 3d 모델 추가

--- src/js/map.js
+++ src/js/map.js
@@ -91,7 +91,148 @@ map.on('load', () => {
         },
         filter: ['==', ['get', 'level'], 2]
     });
+
+
+
+    // 구 생성 시작
+    map.addLayer({
+        id: 'custom_layer',
+        type: 'custom',
+        onAdd: function(map, mbxContext){
+
+            tb = new Threebox(
+                map, 
+                mbxContext,
+                {defaultLights: true}
+            );
+
+        },
+        
+        render: function(gl, matrix){
+            // tb.update();
+        }
+    })
+    // 구 생성 끝
 })
+
+
+
+
+// 3D model 로드 시작
+// parameters to ensure the model is georeferenced correctly on the map
+const modelOrigin = [126.889, 37.5745];
+const modelAltitude = 0;
+const modelRotate = [Math.PI / 2, 0, 0];
+
+const modelAsMercatorCoordinate = maplibregl.MercatorCoordinate.fromLngLat(
+    modelOrigin,
+    modelAltitude
+);
+
+// transformation parameters to position, rotate and scale the 3D model onto the map
+const modelTransform = {
+    translateX: modelAsMercatorCoordinate.x,
+    translateY: modelAsMercatorCoordinate.y,
+    translateZ: modelAsMercatorCoordinate.z,
+    rotateX: modelRotate[0],
+    rotateY: modelRotate[1],
+    rotateZ: modelRotate[2],
+    /* Since our 3D model is in real world meters, a scale transform needs to be
+    * applied since the CustomLayerInterface expects units in MercatorCoordinates.
+    */
+    scale: modelAsMercatorCoordinate.meterInMercatorCoordinateUnits()
+};
+
+const THREE = window.THREE;
+
+// configuration of the custom layer for a 3D model per the CustomLayerInterface
+const customLayer = {
+    id: '3d-model',
+    type: 'custom',
+    renderingMode: '3d',
+    onAdd (map, gl) {
+        this.camera = new THREE.Camera();
+        this.scene = new THREE.Scene();
+
+        // create two three.js lights to illuminate the model
+        const directionalLight = new THREE.DirectionalLight(0xffffff);
+        directionalLight.position.set(0, -70, 100).normalize();
+        this.scene.add(directionalLight);
+
+        const directionalLight2 = new THREE.DirectionalLight(0xffffff);
+        directionalLight2.position.set(0, 70, 100).normalize();
+        this.scene.add(directionalLight2);
+
+        // use the three.js GLTF loader to add the 3D model to the three.js scene
+        const loader = new THREE.GLTFLoader();
+        loader.load(
+            'https://maplibre.org/maplibre-gl-js/docs/assets/34M_17/34M_17.gltf',
+            (gltf) => {
+                this.scene.add(gltf.scene);
+            }
+        );
+        this.map = map;
+
+        // use the MapLibre GL JS map canvas for three.js
+        this.renderer = new THREE.WebGLRenderer({
+            canvas: map.getCanvas(),
+            context: gl,
+            antialias: true
+        });
+
+        this.renderer.autoClear = false;
+    },
+    render (gl, matrix) {
+        const rotationX = new THREE.Matrix4().makeRotationAxis(
+            new THREE.Vector3(1, 0, 0),
+            modelTransform.rotateX
+        );
+        const rotationY = new THREE.Matrix4().makeRotationAxis(
+            new THREE.Vector3(0, 1, 0),
+            modelTransform.rotateY
+        );
+        const rotationZ = new THREE.Matrix4().makeRotationAxis(
+            new THREE.Vector3(0, 0, 1),
+            modelTransform.rotateZ
+        );
+
+        const m = new THREE.Matrix4().fromArray(matrix);
+        const l = new THREE.Matrix4()
+            .makeTranslation(
+                modelTransform.translateX,
+                modelTransform.translateY,
+                modelTransform.translateZ
+            )
+            .scale(
+                new THREE.Vector3(
+                    modelTransform.scale,
+                    -modelTransform.scale,
+                    modelTransform.scale
+                )
+            )
+            .multiply(rotationX)
+            .multiply(rotationY)
+            .multiply(rotationZ);
+
+        this.camera.projectionMatrix = m.multiply(l);
+        this.renderer.resetState();
+        this.renderer.render(this.scene, this.camera);
+        this.map.triggerRepaint();
+    }
+};
+
+map.on('style.load', () => {
+    map.addLayer(customLayer);
+});
+// 3D model 로드 끝
+
+
+
+
+
+
+
+
 
 const popup = new maplibregl.Popup({
     closeButton: true,
